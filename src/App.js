2a88a06a4eb5d8eefa1828240a6b993a33f6e749
nascott <nascott94@outlook.com>
2021-10-18 17:12:58 -0600

    added components and deleted unwanted files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar.js";
+// import Home from "./components/Home.js";
+// import Projects from "./components/Projects.js";
+// import Services from "./components/Services.js";
+// import About from "./components/About.js";
+// import Testimonials from "./components/Testimonials";
+// import Contact from "./components/Contact";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <NavBar />
+    </main>
   );
 }
-
-export default App;
