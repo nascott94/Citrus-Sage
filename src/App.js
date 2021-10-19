b5e2b9f1cb61195ca51ccb2862f957ada3ff326a
nascott <nascott94@outlook.com>
2021-10-19 17:01:08 -0600

    added css styling to topbar and navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React from "react";
-import NavBar from "./components/NavBar.js";
-// import Home from "./components/Home.js";
-// import Projects from "./components/Projects.js";
-// import Services from "./components/Services.js";
-// import About from "./components/About.js";
-// import Testimonials from "./components/Testimonials";
-// import Contact from "./components/Contact";
+import NavBar from "./components/NavBar/NavBar.js";
+// import Home from "./components/Home/Home.js";
+// import Projects from "./components/Projects/Projects.js";
+// import Services from "./components/Services/Services.js";
+// import About from "./components/About/About.js";
+// import Testimonials from "./components/Testimonials/Testimonials.js";
+// import Contact from "./components/Contact/Contact.js";
+// import Footer from "./components/Footer/Footer.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <main>
-      <NavBar />
-    </main>
+    <Router>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <NavBar />
+        {/* <div className="container">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
+          <Route exact path="/services">
+            <Services />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/testimonials">
+            <Testimonials />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+        </div>
+        <Footer /> */}
+      </div>
+    </Router>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,40 +1,36 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-// import { Link } from "react-router-dom";
+import "./NavBar.css";
+import { Link } from "react-router-dom";
 
-export default function ButtonAppBar() {
+export default function NavBar() {
   return (
     <>
       <div id="top-header">
         <div className="container clearfix"></div>
       </div>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Home
-            </Typography>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Projects
-            </Typography>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Services
-            </Typography>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              About
-            </Typography>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Testimonials
-            </Typography>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Contact
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
+      <div className="top">
+        <div className="topCenter">
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="nav-link1 topListItem" to="/">
+                Home
+              </Link>
+              <Link className="nav-link1 topListItem" to="/me">
+                Profile
+              </Link>
+              <Link className="nav-link1 topListItem" to="/signup">
+                Sign Up
+              </Link>
+              <Link className="nav-link1 topListItem" to="/login">
+                Login
+              </Link>
+              <Link className="nav-link1 topListItem" to="/">
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
