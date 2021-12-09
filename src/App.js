ad0a67ac3c0fc88250b28f874697448a1f12795a
nascott <nascott94@outlook.com>
2021-12-09 15:23:00 -0700

    created new components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
 import React from "react";
-import NavBar from "./components/NavBar/NavBar.js";
-import Home from "./components/Home/Home.js";
-// import Projects from "./components/Projects/Projects.js";
-// import Services from "./components/Services/Services.js";
-// import About from "./components/About/About.js";
-// import Testimonials from "./components/Testimonials/Testimonials.js";
-// import Contact from "./components/Contact/Contact.js";
-// import Footer from "./components/Footer/Footer.js";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar.js";
+import Home from "./components/Home.js";
 
 export default function App() {
   return (
-    <Router>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <NavBar />
-        <div className="container">
-          <Route exact path="/">
-            <Home />
-          </Route>
-          {/* <Route exact path="/projects">
-            <Projects />
-          </Route>
-          <Route exact path="/services">
-            <Services />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/testimonials">
-            <Testimonials />
-          </Route>
-          <Route exact path="/contact">
-            <Contact />
-          </Route> */}
-        </div>
-        {/* <Footer /> */}
-      </div>
-    </Router>
+    <div>
+      <NavBar />
+      <Home />
+    </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,6 @@
+import React from "react";
+import logo from "../images/logo.png";
+
+export default function NavBar() {
+  return <>NavBar</>;
+}
