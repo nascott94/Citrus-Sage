e2442fd592e5c2dd0331ebd33c79e99a4e2eaa5e
nascott <nascott94@outlook.com>
2021-12-10 09:48:55 -0700

    working on navbar and home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import NavBar from "./components/NavBar.js";
 import Home from "./components/Home.js";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+`;
 
 export default function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
+      <Container>
+        <NavBar />
+      </Container>
+      <Container>
+        <Home />
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,58 @@
 import React from "react";
-import logo from "../images/logo.png";
+import styled from "styled-components";
+import csLogo from "../images/logo.png";
+
+const Container = styled.div`
+  height: 50px;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Left = styled.div`
+  width: 60%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Image = styled.img`
+  width: 30%;
+`;
+
+const Menu = styled.ul`
+  display: flex;
+  list-style: none;
+  @media only screen and (max-width: 480px) {
+    display: none;
+  }
+`;
+const MenuItem = styled.li`
+  margin-right: 30px;
+  font-size: 20px;
+  color: black;
+`;
 
 export default function NavBar() {
-  return <>NavBar</>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Image src={csLogo} />
+          <Menu>
+            <MenuItem>Home</MenuItem>
+            <MenuItem>About</MenuItem>
+            <MenuItem>Services</MenuItem>
+            <MenuItem>Projects</MenuItem>
+            <MenuItem>Testimonials</MenuItem>
+            <MenuItem>Contact</MenuItem>
+          </Menu>
+        </Left>
+      </Wrapper>
+    </Container>
+  );
 }
